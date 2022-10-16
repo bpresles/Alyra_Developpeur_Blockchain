17b81a83877b71b6683b2fc27a3a017c91cf17c9
Bertrand PRESLES <b.presles@younup.fr>
2022-10-16 10:28:32 +0200

    fix(naming): get proposals list naming was invalid.

--- Solidity - Projet #1 systeme de vote/contracts/Voting.sol
+++ Solidity - Projet #1 systeme de vote/contracts/Voting.sol
@@ -44,7 +44,7 @@ contract Voting is Ownable {
     // Keeping the current status.
     WorkflowStatus private _currentStatus;
 
-    // Winning Proposals.
+    // Winning Proposals that can be viewed by anyone.
     uint[] private _winningProposalsIds;
 
     // Count the total number of votes for control prupose.
